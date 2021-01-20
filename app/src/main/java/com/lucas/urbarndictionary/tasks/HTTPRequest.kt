426ac3b1943cc25295584a2468b9826471ebed3f
Lucas <lucas.perreta@fnetsystem.com.ar>
2021-01-20 17:46:00 -0300

    Added Custom HTTPRequest Async Task

---
+++ app/src/main/java/com/lucas/urbarndictionary/tasks/HTTPRequest.kt
@@ -0,0 +1,54 @@
+package com.lucas.urbarndictionary.tasks
+
+import android.os.AsyncTask
+import android.util.Log
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+class HTTPRequest(val url: String, val callback: (JSONObject?) -> Unit) : AsyncTask<Void, Int, JSONObject?>() {
+
+    companion object {
+
+        fun make(url: String, callback: (JSONObject?) -> Unit) {
+            HTTPRequest(url, callback).execute()
+        }
+
+    }
+
+    override fun doInBackground(vararg p0: Void?): JSONObject? {
+        val client: HttpURLConnection = URL(url).openConnection() as HttpURLConnection
+        client.requestMethod = "GET"
+        client.setRequestProperty(
+            "X-RapidAPI-Key",
+            "kRLTeO9IyCmshsdmTBuOQmHULNrkp1gzQHAjsnzB8w9qhonso1"
+        )
+        client.setRequestProperty(
+            "X-RapidAPI-Host",
+            "mashape-community-urban-dictionary.p.rapidapi.com"
+        )
+        val builder = StringBuilder()
+
+        if (client.responseCode != 200) {
+            return null
+        }
+
+        val reader = BufferedReader(InputStreamReader(client.inputStream, "utf-8"))
+        var responseLine: String?
+        while (reader.readLine().also { responseLine = it } != null) {
+            builder.append(responseLine!!.trim { it <= ' ' })
+        }
+        reader.close()
+
+        return JSONObject(builder.toString())
+    }
+
+    override fun onPostExecute(result: JSONObject?) {
+        super.onPostExecute(result)
+        callback(result)
+        Log.e("AAA", url)
+    }
+
+}
